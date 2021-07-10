aadfe5f2f1283b381f70db1f5e024166c92c3132
amv1017 <amv1017@mail.ru>
2021-07-10 15:56:48 +0300

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-mod strings;
+mod vectors;
+// mod arrays;
+// mod tuples;
+// mod strings;
 // mod types;
 // mod vars;
 // mod print;
 
 fn main() {
-  strings::run();
+  vectors::run();
+  // arrays::run();
+  // tuples::run();
+  // strings::run();
   // types::run();
   // vars::run();
   // print::run();
